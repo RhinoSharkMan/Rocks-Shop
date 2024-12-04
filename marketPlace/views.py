79bd04b5b29af956598b1e39f1f3fe709861265e
JoshO13 <joshhockey19@gmail.com>
2024-12-03 19:07:25 -0600

    Created signup form

--- marketPlace/views.py
+++ marketPlace/views.py
@@ -1,6 +1,7 @@
 from unicodedata import category
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from item.models import Category, Item
 # from django.http import HttpResponse
 from .forms import SignupForm
@@ -18,8 +19,17 @@ def contact(requst):
     return render(requst, 'marketPlace/contact.html')
 
 def signup(request):
-    form = SignupForm
-
-    return render(request, 'marketPlace/signup.html', {
-        'form': form
-    })
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('marketPlace:index')
+        else:
+            return render(request, 'marketPlace/signup.html', {'form': form})
+    else:
+        form = SignupForm()
+        return render(request, 'marketPlace/signup.html', {'form': form})
